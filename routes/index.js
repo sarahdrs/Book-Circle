dc99c9d30e7245df28d480e93ca3d9bedfc0f1f6
StephGidde <steph.gidde@gmx.de>
2019-01-19 16:58:27 +0100

    created dashboard(not finished)

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.get("/:userid/dashboard", (req, res) => {
+  User.find({
+    _id: req.params._id
+  })
+    .then(user => {
+      res.render("User/dashboard", { user });
+    })
+    .catch(err => {
+      console.log(err, "there was an error");
+    });
 });
 
 module.exports = router;
