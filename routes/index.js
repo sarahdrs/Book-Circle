095dd358a5baff58ab80d8563c12509088c1d34b
Axel Heinz <axelmheinz@gmail.com>
2019-01-25 17:38:44 +0100

    rendering find-book.hbs with api data

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
-const books = require('google-books-search');
+const books = require("google-books-search");
 const ensureLogin = require("connect-ensure-login");
-
 
 /* GET home page */
 router.get("/", (req, res, next) => {
@@ -21,43 +20,43 @@ router.get("/dashboard", (req, res, next) => {
 
 var options = {
   key: "",
-  field: 'title',
+  field: "title",
   offset: 0,
-  limit: 10,
-  type: 'books',
-  // order: 'relevance',
+  limit: 15,
+  type: "books",
+  order: "relevance"
 };
 
 router.get("/find-book", (req, res, next) => {
-  if (req.query.book){
-    books.search(req.query.book, options, function (error, results) {
-      let results3 = results
-      let message = ""
-      console.log(results)
+  if (req.query.book) {
+    books.search(req.query.book, options, function(error, results) {
+      let results3 = results;
+      let message = "";
+      console.log(results);
       if (results.length === 0) {
-        message = "Try Harry Potter"
-        results3 = [{title: ""}]
+        message = "Try Harry Potter";
+        results3 = [{ title: "" }];
       }
       if (!error) {
-        res.render('User/find-book', {items: results3[0].items, message},
-        // {layout: "User/layout"}
-        )
-  
+        res.render(
+          "User/find-book",
+          { results, message, layout: "User/layout" }
+        );
       } else {
         console.log(error);
       }
-    })
+    });
+  } else {
+    res.render("User/find-book", {
+      layout: "User/layout"
+    });
   }
-  else {res.render("User/find-book", {
-    layout: "User/layout"
-  });}
 });
-
 
 router.get("/:userid/dashboard", (req, res) => {
   User.find({
-      _id: req.params._id
-    })
+    _id: req.params._id
+  })
     .then(user => {
       res.render("User/dashboard", {
         user
@@ -65,22 +64,16 @@ router.get("/:userid/dashboard", (req, res) => {
     })
     .catch(err => {
       console.log(err, "there was an error");
-    })
-  })
+    });
+});
 // successfully logged in (private routes)
 
-router.get(
-  "/dashboard",
-  ensureLogin.ensureLoggedIn("signin"),
-  (req, res) => {
-    res.render("User/dashboard", {
-      layout: "User/layout",
-      title: "Hello, Username"
-    });
-  }
-);
-    
-
+router.get("/dashboard", ensureLogin.ensureLoggedIn("signin"), (req, res) => {
+  res.render("User/dashboard", {
+    layout: "User/layout",
+    title: "Hello, Username"
+  });
+});
 
 // for testing profile editing
 router.get("/editprofile", (req, res, next) => {
@@ -89,6 +82,5 @@ router.get("/editprofile", (req, res, next) => {
     title: "Hello, Username"
   });
 });
-
 
 module.exports = router;
