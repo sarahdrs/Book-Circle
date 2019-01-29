cfcbcf70e00d28d7272533d26f02d537a566fa0f
StephGidde <steph.gidde@gmx.de>
2019-01-29 21:19:35 +0100

    user dashboard add favorites works

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,12 +1,13 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    console.log("IronGenerator JS imported successfully!");
+  },
+  false
+);
 
-  console.log('IronGenerator JS imported successfully!');
-
-}, false);
-
-
-//function to toggle "Results:" in /find-book
-$("#searchBtn").click(function(){
-  $("#results").css('display','none');
-  console.log("button pressed")
-});
+// //function to toggle "Results:" in /find-book
+// $("#searchBtn").click(function(){
+//   $("#results").css('display','none');
+//   console.log("button pressed")
+// });
